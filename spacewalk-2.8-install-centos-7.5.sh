24b33eeb7a34596cba19020bf9e9551911c32cca
catonrug <aigars.kadikis@gmail.com>
2018-06-27 22:06:09 +0300

    next

--- spacewalk-2.8-install-centos-7.5.sh
+++ spacewalk-2.8-install-centos-7.5.sh
@@ -68,16 +68,6 @@ postgresql-setup initdb
 
 #start postgresql
 systemctl start postgresql
-if [ $? -eq 0 ]; then
-
-su - postgres -c psql <<<"update pg_database set datistemplate=false where datname='template1';"
-su - postgres -c psql <<<"drop database template1;"
-su - postgres -c psql <<<"create database template1 with owner=postgres encoding='UTF-8' lc_collate='en_US.utf8' lc_ctype='en_US.utf8' template template0;"
-su - postgres -c psql <<<"update pg_database set datistemplate=true where datname='template1';"
 
 #spacewalk silent install
 spacewalk-setup --answer-file=/root/spacewalk-answer-file
-
-else
-echo postgresql is not running
-fi
